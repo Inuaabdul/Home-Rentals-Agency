37de5e9cf776b68c33554820c9b0bc0eb1804ddf
Inuaabdul <belloabdurrahman72@gmail.com>
2023-09-19 00:24:21 +0100

    card javascript

--- scripts/card.js
+++ scripts/card.js
@@ -16,23 +16,23 @@ cardft.classList.add('card-ft')
 const ft1 = document.createElement('div')
 ft1.classList.add('ft1')
 const svg = document.createElement('img')
-svg.src = './imgs/s.svg'
+
 const fh4 = document.createElement('h4')
-fh4.innerText = '4'
+
 
 const ft2 = document.createElement('div')
 ft2.classList.add('ft2')
 const svg2 = document.createElement('img')
-svg2.src = './imgs/t.svg'
+
 const fh42 = document.createElement('h4')
-fh42.innerText = '2'
+
 
 const ft3 = document.createElement('div')
 ft3.classList.add('ft3')
 const svg3 = document.createElement('img')
-svg3.src = './imgs/u.svg'
+
 const fh43 = document.createElement('h4')
-fh43.innerText = '2'
+
 
 
 Mdiv.appendChild(cardPic)
@@ -54,35 +54,40 @@ ft3.appendChild(fh43)
 const addCard = () => {
     properties.forEach((Element) => {
 
+        const image = document.createElement('img')
+        image.style.height = '271px'
+        // const blob = new Blob([Element.drag]);
+        const url = URL.createObjectURL(Element.drag)
+        image.src = url
 
-        const input = document.querySelector('#photo')
-        input.addEventListener('change', () => {
-            const reader = new FileReader()
-            reader.addEventListener('load', () => {
-                const img = document.createElement('img')
-                img.src = reader.result
-                const source = document.querySelector('.card-pic')
-                source.appendChild(img)
-            })
-            reader.readAsDataURL(input.files[0])
-        })
+        cardPic.appendChild(image)
+
+
 
 
 
         htwo.innerText = Element.address
         hthree.innerText = Element.room
         hfour.innerText = Element.price
+        svg.src = './imgs/s.svg'
+        fh4.innerText = '4'
+        svg2.src = './imgs/t.svg'
+        fh42.innerText = '2'
+        svg3.src = './imgs/u.svg'
+        fh43.innerText = '2'
     })
 }
 
 
-addCard()
-document.querySelector('.lists').appendChild(Mdiv)
+
+
+// addCard()
+// document.querySelector('.lists').appendChild(Mdiv)
 
 
 
 const form = document.querySelector('.add-props form')
-const btn = document.querySelector('.bnn')
+const btn = document.querySelector('.bn')
 
 btn.addEventListener('click', (e) => {
     e.preventDefault()
@@ -96,7 +101,7 @@ btn.addEventListener('click', (e) => {
     const room = document.querySelector('#room')
     const price = document.querySelector('#price')
     const description = document.querySelector('textarea')
-    const drag = document.querySelector('#drag')
+    const drag = document.querySelector('#photo')
 
     const data = {
         name: name.value,
@@ -107,7 +112,7 @@ btn.addEventListener('click', (e) => {
         room: room.value,
         price: price.value,
         description: description.value,
-        drag: drag.value
+        drag: drag.files[0]
     }
     name.value = ''
     address.value = ''
@@ -120,6 +125,9 @@ btn.addEventListener('click', (e) => {
     drag.value = ''
 
     properties.push(data)
+    document.querySelectorAll('.card').innerHTML = ''
     addCard()
 
 })
+
+document.querySelector('.lists').appendChild(Mdiv)
